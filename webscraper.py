9d322b04ed0395f09f01ac1168a591c9471235eb
Dujma7 <zvonkodujmovic07@gmail.com>
2024-09-24 08:08:46 +0200

    Radula v1.1

--- webscraper.py
+++ webscraper.py
@@ -2,21 +2,33 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-url = "https://www.elementalatina.com/2014/06/vjezba-32.html"
-html = requests.get(url)
-search = BeautifulSoup(html.content, "html.parser")
-vocabulary = search.find(id="rj")
-words = vocabulary.find_all("li")
-regex = r'<p class="(?:word|word-old)">(.*?)</p>(.*?)</li>'
-wordsClean = re.findall(regex, str(words))
-wordList = []
-for word in wordsClean:
-    wordList.append([word[0]+": "+word[1]])
 
-filename = "rijeci.txt"
-with open("rijeci.txt", "w", encoding="utf-8") as file:
-    for words in wordList:
-        file.write(words[0]+"\n")
+def radula(urlNumber):
+    url = "https://www.elementalatina.com/2014/06/vjezba-"+str(urlNumber)+".html"
+    html = requests.get(url)
+    search = BeautifulSoup(html.content, "html.parser")
+    vocabulary = search.find(id="rj")
+    words = vocabulary.find_all("li")
+    wordList = []
+    filename = "rijeci-"+str(urlNumber)+".txt"
 
+    def cleanWords():
+        regex = r'<p class="(?:word|word-old)">(.*?)</p>(.*?)</li>'
+        wordsClean = re.findall(regex, str(words))
+        for word in wordsClean:
+            wordList.append([word[0]+": "+word[1]])
+
+    def createFile():
+        with open(filename, "x", encoding="utf-8") as file:
+            file.write("RIJECI VJEZBE BR. "+str(urlNumber)+"\n")
+
+    def wordsToTxt():
+        with open(filename, "a", encoding="utf-8") as file:
+            for word in wordList:
+                file.write(word[0]+"\n")
+
+    cleanWords()
+    createFile()
+    wordsToTxt()
 
 
